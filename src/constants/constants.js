4f37a1602fa4f398dfb0df539818f67caa02c41c
bhanuprakashsfdc <bhanusunny7@gmail.com>
2025-05-04 12:11:52 +0530

    pushing latest constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -8,6 +8,9 @@ export const APP_NAME = "thetimeis.net";
 export const TIMEIN = "time-in/";
 export const TIMENOW = "time-now/";
 export const LOCALTIME = "local-time/";
+export const CURRENTTIME = "current-time/";
+export const LOCALTIMEIN = "local-time-in/";
+export const CURRENTTIMEIN = "current-time-in/";
 export const WHATISTHETIMERIGHTNOWIN = "what-is-the-time-right-now-in/";
 export const WHATTIMEITISIN = "what-time-it-is-in/";
 
